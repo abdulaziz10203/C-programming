c6ae79be8bbb937fe9cdf630e043c4fc7f0876bf
abdulaziz10203 <113228544+abdulaziz10203@users.noreply.github.com>
2022-09-10 11:26:28 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    int x,fact=1,n;
+
+    printf("Enter a number to find factorial: ");
+
+    scanf("%d",&n);
+    if (isdigit(n)){
+    	for(x=1;x<=n;x++)
+
+        fact=fact*x; 
+
+    printf("Factorial of %d is: %d",n,fact);
+	}
+	else{
+		printf("input is invalid");
+	}
+	
+
+    
+
+    return 0;
+
+}
